f355dfb152fa1b30d71b2719c6c0bbcffe9752a0
Mikhail Metrikin <metrikin@gmail.com>
2018-08-20 21:30:08 -0700

    SAVEPOINT

--- functions.py
+++ functions.py
@@ -10,3 +10,8 @@ def print_void():
 
 print_two('Misha', 'Met')
 print_void()
+
+
+def cheese_and_crackers(cheese, crackers):
+    print(f'You have {cheese} cheeses!')
+    print(f'You ahve {crackers} crackers')
